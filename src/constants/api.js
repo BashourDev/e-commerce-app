3c2d0212428865f9d3d3c5c7f87a808921f8606f
BashourDev <bashour.atrini.dev@gmail.com>
2023-10-05 17:03:28 +0400

    reverted back the old stuff

--- src/constants/api.js
+++ src/constants/api.js
@@ -6,4 +6,4 @@ const SHOPIFY_CARD_VAULT_URL = "https://elb.deposit.shopifycs.com/sessions";
 //   require("../../.env.json").STOREFRONT_ACCESS_TOKEN;
 
 export { SHOPIFY_CARD_VAULT_URL };
-// export { STOREFRONT_API_URL, STOREFRONT_ACCESS_TOKEN };
+export { STOREFRONT_API_URL, STOREFRONT_ACCESS_TOKEN } from "../../.env.json";
